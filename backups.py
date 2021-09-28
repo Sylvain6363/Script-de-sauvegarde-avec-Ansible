6f8cff4d0160ba26803101fef188387b42f4830f
sylvain6363 <vigier.sylvain@yahoo.fr>
2021-09-28 14:04:58 +0200

    v5

--- backups.py
+++ backups.py
@@ -1,11 +1,10 @@
 ###########################################################################################
 #
 # TITRE          : Script de sauvegarde avec Ansible
-
 # DESCRIPTION    : 
 #  Sauvegarde un répertoire dans un serveur ciblé. La sauvegarde est mis au format zip
-#  et également copié dans un répertoire FTP local. A chaque sauvegarde deposée, quand 
-# la quantité de sauvegarde maximum est dépassée, la sauvegarde la plus vieille est 
+#  et également copiée dans un répertoire FTP local. A chaque sauvegarde déposée, quand 
+#  la quantité de sauvegarde maximum est dépassée, la sauvegarde la plus vieille est 
 #  supprimée. La sauvegarde zip d'origine sur le serveur ciblé est aussi supprimée.
 #
 # AUTEUR         : Sylvain VIGIER
@@ -33,7 +32,7 @@ ftp = FTP()
 srv = 'SRV-WORDPRESS'
 # variable nom du dossier à sauvegarder ( le playbook modifie cette variable )
 nomdossier = "bitnami"
-# variable qui correspond a /nomdossier ( le playbook modifie cette variable )
+# variable qui correspond a /nomdossier
 Dossier = f"/{nomdossier}"
 # variable chemin du dossier à sauvegarder
 Chemindossier = Path(Dossier) 
@@ -55,25 +54,29 @@ QUANTITE_DE_SAUVEGARDE_MAX = 7
 def listedossierasauvegarder(dossier):
     # créer une variable fichierdossier a partir de la fonction recupfichierdansdossier
     fichierdossier = recupfichiersdansdossier(dossier)
-    # pour tous le contenu dans fichierdossier, affiche son contenu
+    # pour tous le contenu dans fichierdossier 
     for nom_fichier in fichierdossier:
+        # affiche son contenu
         print(nom_fichier)
-    print("")
+    
 
 # fonction de création de l'archive zip
 # fichierliste represente le contenu du dossier à sauvegarder
 # nomarchive est le nom de fichier de l'archive avec un chemin complet
 def creerArchive(fichierliste, nomarchive):
-    # essaye ceci sinon le script s'arrete
+    # essaye ceci 
     try:
         # variable a pour créer une archive qui portera le nom nomarchive avec une compression standard deflated
         a = zipfile.ZipFile(nomarchive, 'w', zipfile.ZIP_DEFLATED)
-        # pour tout le contenu dans fichierliste, ecrit le dans l'archive
-        for f in fichierliste:  
+        # pour tout le contenu dans fichierliste
+        for f in fichierliste:
+            # ecrit ce contenu dans l'archive  
             a.write(f)
+        # fermer le fichier archive
         a.close()
         # si l'archive est créée alors on continu
         return True
+    # sinon le script s'arrete
     except: return False
 
 # fonction qui récupère tous le contenu du dossier à sauvegarder, qui l'insert dans un dictionnaire, et renvoit son contenu.
@@ -81,10 +84,11 @@ def creerArchive(fichierliste, nomarchive):
 def recupfichiersdansdossier(dossier):
     # tous les fichiers du dossier à sauvegarder iront dans ce dictionnaire
     fichiersdossier = []
-    # parcourir le répertoire et les sous-répertoires
+    # pour le répertoire et les sous-répertoires dans le dossier 
     for racine, dossiers, fichiers in os.walk(dossier):
-        # joindre les deux chaînes afin de former le chemin de fichier complet.
+        # pour les nomfichier dans les fichiers
         for nomfichier in fichiers:
+            # joindre les deux chaînes afin de former le chemin de fichier complet.
             cheminfichier = os.path.join(racine, nomfichier)
             # ajouter le contenu au dictionnaire fichiersdossier
             fichiersdossier.append(cheminfichier)
@@ -103,48 +107,59 @@ def obtenirsauvegardespresentes():
 
 # fonction qui permet d'afficher la liste des sauvegardes de manière plus lisible
 def afficherlistesauvegardes():
-    # permet d'afficher seulement les noms complets des sauvegardes
+    # permet d'afficher seulement les sauvegardes avec l'extension ".zip" dans l'ordre
+    # pour les lignes dans l'ordre de chaque sauvegarde du dossier FTP
     for ligne in sorted(obtenirsauvegardespresentes()):
+        # x represente le delimiteur "."
         x = ligne.split(".")
+        # le formats represente "zip"
         formats=["zip"]
+        # si le delimiteur "." est juste avant le formats "zip" (.zip)
         if x[-1] in formats:
+            # alors affiche toutes ces lignes avec un tiret devant
             print("-", ligne)
-    print("")
+    
 
-# fonction qui supprime la plus ancienne des 6 sauvegardes maximum autorisées deja presentes sur le serveur FTP    
+# fonction qui supprime la plus ancienne des 7 sauvegardes maximum autorisées deja presentes sur le serveur FTP    
 def suppressionsauvegarde():
     # variable representant la liste dans l'ordre des sauvegardes presentes dans le répertoire FTP
     liste_des_sauvegardes = list(sorted(obtenirsauvegardespresentes()))  
     # tant que la liste des sauvegardes est supérieur ou égal au nombre maximum de sauvegardes autorisées à être stockées
     while len(liste_des_sauvegardes) >= QUANTITE_DE_SAUVEGARDE_MAX:  
-        # supprimer la sauvegarde la plus ancienne
+        # variable representant la sauvegarde la plus ancienne (en position 0)
         sauvegarde_a_supprimer = liste_des_sauvegardes.pop(0)
+        # supprimer le contenu de la variable sauvegarde_a_supprimer (sauvegarde la plus ancienne)
         ftp.delete(sauvegarde_a_supprimer)
 
 # fonction qui créé le dossier (porte le meme nom que le repertoire a sauvegarder) ou envoyer les sauvegardes sur le serveur FTP
 # nomdudossier est le nom du répertoire à sauvegarder 
 def repertoiresauvegarde(nomdudossier):
-    # Si le nom de dossier n'existe pas dans le dossier racine du serveur FTP, il le créé et se place par défaut sur celui-ci
+    # si le nom de dossier n'existe pas dans le dossier racine du serveur FTP
     if not nomdudossier in ftp.nlst():
+        # le dossier est créé
         ftp.mkd(nomdudossier)
+        # se place par défaut sur celui-ci
         ftp.cwd(nomdudossier)
-    # Sinon il existe déjà et se place par défaut sur celui-ci
+    # sinon 
     else:
+        # le dossier existe déjà et se place par défaut sur celui-ci
         ftp.cwd(nomdudossier)
 
 # fonction qui stock la sauvegarde zippée dans le répertoire FTP 
 def stocklasauvegarde(arc):
+    # stock l'archive zippée "arc" dans le repertoire FTP 
     ftp.storbinary('STOR ' + arc, open(arc, 'rb'))
 
 # fonction principale
 def main():
-    # affiche un titre Sauvegardes Zip FTP
+    # affiche un titre Sauvegardes Zip FTP avec les commandes print()
     print("---------------------------")
     print("---------------------------")
     print("--  Sauvegardes Zip FTP  --")
     print("---------------------------")
     print("------ " + (srv) + " ------")
     print("\n=====")
+
     print("Les fichiers suivants seront compressés:")
     # lance la fonction listedossierasauvegarder qui point sur le dossier à sauvegarder,
     # il affiche tous les chemins des fichiers du dossier
@@ -185,8 +200,12 @@ def main():
     # lance la fonction qui stock la sauvegarde dans le dossier FTP en utilisant le parametre archive, c'est a dire le nom que portera l'archive
     stocklasauvegarde(archive)
     print("=====\n")
+    print("=====")
+    print("Obtenir la liste des Sauvegardes à jour")
     # lance a nouveau la fonction afficherlistesauvegardes qui affiche la liste des sauvegardes à present modifiées depuis le serveur FTP
     afficherlistesauvegardes()
+    # affiche le nombre de fichiers trouvés dans le dossier contenant les sauvegardes après la suppression de l'ancienne et l'ajout de la nouvelle
+    print(str(len(obtenirsauvegardespresentes())) + " fichiers trouvés")
     print("=====")
     print("Fermeture de la Connexion")
     # deconnexion au serveur FTP
